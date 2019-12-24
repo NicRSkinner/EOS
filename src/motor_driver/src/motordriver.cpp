9ba86c822f6b7415585f2ed5ce730f7e095c8dd1
Nick Skinner <nicholas.skinner95@gmail.com>
2019-12-24 06:41:54 -0600

    Added eos_msgs depends for motor_driver pkg

--- src/motor_driver/src/motordriver.cpp
+++ src/motor_driver/src/motordriver.cpp
@@ -1,6 +1,12 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+
 #include "escdriver.h"
+#include "eos_msgs/gamepad_output.h"
 
 int main(int argc, char *argv[])
 {
-
+    // Check to make sure this works.
+    //ESCDriver rwMotor(0, 500000lu, 1500000lu, 2000000lu);
 }
